90cb9f91f83bd9a95c40cfc6c128868b320909fe
Rostislav Lyupa <supfire2013@gmail.com>
2023-10-10 14:21:22 +0300

    implement gauss max element method - Lab3

--- Lab3/gauss.py
+++ Lab3/gauss.py
@@ -39,3 +39,29 @@ def rectangle(matrix: np.array):
       matrix[j] -= matrix[i] * matrix[j, i]
 
   return matrix[:, -1]
+
+def max_element(matrix: np.array):
+  if np.linalg.matrix_rank(matrix[:, :-1]) < np.linalg.matrix_rank(matrix):
+    raise ValueError("system has no solutions")
+
+  if np.linalg.matrix_rank(matrix) < matrix.shape[1] - 1:
+    raise ValueError("system is undetermined")
+    
+  for k in range(matrix.shape[0]):
+    max_elem = max([abs(elem) for elem in matrix[:, k]])
+    max_index = np.where(abs(matrix[:, k]) == max_elem)[0][0]
+    matrix[k], matrix[max_index] = matrix[max_index], matrix[k].copy()
+    
+    for i in range(k + 1,matrix.shape[0]):
+      for j in range(k + 1, matrix.shape[1]):
+        matrix[i, j] = matrix[i, j] - (matrix[k, j] * matrix[i, k] / matrix[k, k])   
+
+    for elem in range(k + 1, matrix.shape[0]):
+      matrix[elem, k] = 0
+    matrix[k] /= matrix[k, k]
+
+  for i in range(matrix.shape[0] - 1, 0, -1):
+    for j in range(i - 1, -1, -1):
+      matrix[j] -= matrix[i] * matrix[j, i]
+
+  return matrix[:, -1]
