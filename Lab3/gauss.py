cae94be0233ef2cac429bf840a5f412ccc9a752e
Lyupa Rostislav <supfire2013@gmail.com>
2023-10-10 13:48:27 +0300

    implement gauss rectangle method - Lab3

--- Lab3/gauss.py
+++ Lab3/gauss.py
@@ -17,3 +17,25 @@ def gauss(matrix: np.array):
       matrix[j] -= matrix[i] * matrix[j, i]
 
   return matrix[:, -1]
+
+def rectangle(matrix: np.array):
+  if np.linalg.matrix_rank(matrix[:, :-1]) < np.linalg.matrix_rank(matrix):
+    raise ValueError("system has no solutions")
+
+  if np.linalg.matrix_rank(matrix) < matrix.shape[1] - 1:
+    raise ValueError("system is undetermined")
+  
+  for k in range(matrix.shape[0]):
+    for i in range(k + 1, matrix.shape[0]):
+      for j in range(k + 1, matrix.shape[1]):
+        matrix[i, j] = matrix[i, j] - (matrix[k, j] * matrix[i, k] / matrix[k, k])
+
+    for elem in range(k + 1, matrix.shape[0]):
+      matrix[elem, k] = 0
+    matrix[k] /= matrix[k, k]
+
+  for i in range(matrix.shape[0] - 1, 0, -1):
+    for j in range(i - 1, -1, -1):
+      matrix[j] -= matrix[i] * matrix[j, i]
+
+  return matrix[:, -1]
--- Lab3/test.py
+++ Lab3/test.py
@@ -86,7 +86,82 @@ class TestGauss(unittest.TestCase):
         gauss.gauss(matrix)
       self.assertTrue("system has no solutions" in str(context.exception))
 
+class TestGaussRectangle(unittest.TestCase):
+  def test_correct(self):
+    eps = 1e-6
+
+    matrices = [
+      np.array([
+        [1, 2,  1,  8],
+        [3, 2,  1, 10],
+        [4, 3, -2,  4]
+      ], dtype=float),
+      np.array([
+        [3,  2, -5, -1],
+        [2, -1,  3, 13],
+        [1,  2, -1,  9]
+      ], dtype=float),
+      np.array([
+        [3, 0]
+      ], dtype=float),
+    ]
+
+    solutions = [
+      [1, 2, 3],
+      [3, 5, 4],
+      [0]
+    ]
+
+    for i in range(len(matrices)):
+      print(gauss.rectangle(matrices[i]))
+      self.assertTrue(isSolution(solutions[i], gauss.rectangle(matrices[i]), eps))
+  
+  def test_undetermined(self):
+    matrices = [
+      np.array([
+        [2, -1,  3,  -5, 1],
+        [1, -1, -5,   0, 2],
+        [3, -2, -2,  -5, 3],
+        [7, -5, -9, -10, 8]
+      ], dtype=float),
+      np.array([
+        [2, -1,  3,  -5, 1],
+        [1, -1, -5,   0, 2]
+      ], dtype=float),
+      np.array([
+        [2, 1, 1],
+        [4, 2, 2]
+      ], dtype=float),
+      np.array([
+        [0, 0],
+      ], dtype=float),
+    ]
+
+    for matrix in matrices:
+      with self.assertRaises(ValueError) as context:
+        gauss.rectangle(matrix)
+      self.assertTrue("system is undetermined" in str(context.exception))
+
+  def test_no_solution(self):
+    matrices = [
+      np.array([
+        [1, -1,  3],
+        [1, -1, -5]
+      ], dtype=float),
+      np.array([
+        [1, -1,  3],
+        [2, -2, 7]
+      ], dtype=float),
+      np.array([
+        [0, 1]
+      ], dtype=float)
+    ]
+
+    for matrix in matrices:
+      with self.assertRaises(ValueError) as context:
+        gauss.rectangle(matrix)
+      self.assertTrue("system has no solutions" in str(context.exception))
+
 
 if __name__ == "__main__":
   unittest.main()
-  
